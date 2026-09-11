aa0ae3e922089bd3e83289fbbfa51d105ed03b24
yangzihao260-boop <yangzihao260@gmail.com>
2026-09-11 21:32:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/find-the-leaf/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
